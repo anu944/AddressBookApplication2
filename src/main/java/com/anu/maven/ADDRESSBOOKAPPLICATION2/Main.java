47071db4768d23563b24026f4fda70ef9f2c1f99
anu944 <bapathuanu@gmail.com>
2025-08-08 22:40:38 +0530

    Update Main.java

--- src/main/java/com/anu/maven/ADDRESSBOOKAPPLICATION2/Main.java
+++ src/main/java/com/anu/maven/ADDRESSBOOKAPPLICATION2/Main.java
@@ -1,5 +1,9 @@
 package com.anu.maven.ADDRESSBOOKAPPLICATION2;
 
-public class Main {
+import controller.ContactController;
 
+public class Main {
+    public static void main(String[] args) {
+        new ContactController().start();
+    }
 }
